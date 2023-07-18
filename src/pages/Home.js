9877d209e4330b8b44e8e9d8ad81a5aebc8d1e03
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-18 23:17:22 +0900

    소셜 로그인 이미지 추가 및 반영

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,27 @@ const Login = () => {
     window.location.href="./signin"
   }
 
+  const Rest_api_key = 'e6c2fe139670b147caaf750b558a4750'
+  const redirect_uri = 'http://192.168.45.119:3000/kakao-login'
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+  function kakao(){
+    window.location.href = kakaoURL
+  }
+
+  function naver(){
+    window.location.href = '/'
+  }
+
   return (
     <div class="flex justify-center items-center text-center min-h-screen">
       <div class="">
         <div class="pb-8 font-bold text-2xl">부산시 문화예술 지도</div>
         <div class="grid grid-rows-4 gap-3">
-          <div onClick={local}>로컬 로그인</div>
-          <div>카카오 로그인</div>
-          <div>네이버 로그인</div>
+          <button onClick={local} class="mx-2 rounded border-solid border-2 border-indigo-500 bg-indigo-500">
+            <p class="text-white font-bold">문화</p>
+          </button>
+          <img onClick={kakao} class="px-2" src="images/kakao_login_medium_narrow.png"></img>
+          <img onClick={naver} class="px-2 h-11 w-full" src="images/btnG_완성형.png"></img>
         </div>
       </div>
     </div>
