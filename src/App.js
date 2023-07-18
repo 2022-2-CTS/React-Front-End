a4b954f4a860ebf9ff8d7452b477f3c5d0fb9b57
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-18 18:14:57 +0900

    테일윈드 적용 완료
    https://velog.io/@hokim/React-2-React-Tailwind-%EC%84%B8%ED%8C%85
    
    npm install -D tailwindcss postcss autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import "./index.css";
 
 import Home from './pages/Home.js'
 import SignIn from './pages/SignIn.js'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <h1>부산 문화 예술 지도</h1>
+      <h1 className="text-3xl font-bold underline">부산 문화 예술 지도</h1>
+      <p className="font-serif font-bold">The quick brown fox</p>
+      <div className="text-3xl font-bold underline">hello</div>;
     </div>
   ); 
 };
