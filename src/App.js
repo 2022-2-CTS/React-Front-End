3cf2b51c0ca46d60580d6ea10f0c98ea92eef05a
leejyohan <86225337+leejyohan@users.noreply.github.com>
2023-07-19 22:22:33 +0900

    📝 ShareInfo 정보공유 페이지 생성 / 라우팅

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./index.css";
 import Home from './pages/Home.js'
 import SignIn from './pages/SignIn.js'
 import SignUp from './pages/SignUp.js'
+import ShareInfo from './info/ShareInfo';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home/shareinfo" element={<ShareInfo />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/info/ShareInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShareInfo = () => {
+  return (
+    <div>
+        정보공유페이지
+    </div>
+  );
+};
+
+export default ShareInfo;
