3c09108e0f25a46cdd7e639736f900c3bcedfcb6
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-24 20:25:43 +0900

    ✨ 카카오 로그인 라우팅 설정

--- src/socials/KakaoLogin.js
+++ src/socials/KakaoLogin.js
@@ -4,62 +4,32 @@ import axios from 'axios'
 const KakaoLogin = () => {
     
     const loginCode = new URL(window.location.href).searchParams.get('code')
-
-    // var details = {
-    //     "grant_type" : "authorization_code",
-    //     "client_id" : 'e6c2fe139670b147caaf750b558a4750', //REST API KEY
-    //     "redirect_uri" : 'http://localhost:3000/kakao-login',
-    //     "code" : loginCode
-    // }
-
-    // var formBody = []
-
-    // for (var property in details){
-    //     var encodedKey = encodeURIComponent(property)
-    //     var encodedValue = encodeURIComponent(details[property])
-    //     formBody.push(encodedKey + "=" + encodedValue);
-    // }
-    // formBody = formBody.join("&")
-
-    // console.log(formBody)
-
     
     useEffect(() => {
         if (loginCode){
-            response()
+            do_Login()
         }
     }, [])
 
-    // const getKakaoTokenHandler = () => {
-
-    //     axios({
-    //         method : 'post',
-    //         url : 'https://kauth.kakao.com/oauth/token/',
-    //         headers : {
-    //             'Content-type' : 'application/x-www-form-urlencoded;charset=utf-8'
-    //         },
-    //         data : formBody
-    //     })
-    //         .then(response => {
-    //             const jwtToken = response.data.jwt;
-    //             console.log("JWT발급 성공", jwtToken)
-    //         })
-    //         .catch(error => {
-    //             console.log('JWT토큰 발급에 실패했습니다.', error)
-    //     }) 
-    // }
-
-    const response = () => {
-        axios.post('http://localhost:8080/api/index/test',{
+    const do_Login = () => {
+        axios.post('http://localhost:3004/api/index/test',{
             code : loginCode
         })
-        .then(res => console.log(res.data))
+        .then((req) => {
+            console.log(req.data);
+            if (req.data == "로그인 성공!!"){
+                window.location.href = '/'
+            }else{
+                window.location.href = '/signin'
+                console.log("로그인 실패")
+            }
+        })
         .catch(err => console.log(err))
     }
 
     return(
         <div>
-            { loginCode }
+            { loginCode } 
         </div>
     );
 };
