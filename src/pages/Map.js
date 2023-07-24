175b12714401de19f9e3ef941bd957dc72956f27
Miseon <soup7912@gmail.com>
2023-07-24 11:33:33 +0900

    ✨ Feat: kakao map api 사용

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
+
+// 스크립트로 kakao map api를 심어서 가져오면, window 전역 객체에 들어가게 된다.
+// 함수형 컴포넌트에서는 바로 인식하지 못하므로, kakao 객체를 인지시키고자 상단에 선언해둔다.
+const { kakao } = window; // window 내 kakao 객체를 빼와서 사용
 
 const Map = () => {
-  return (
-    <div>
-        Map Page
-    </div>
-  );
+    useEffect(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new kakao.maps.LatLng(35.14932, 129.11462, 16),
+          level: 6,
+        };
+
+
+        const map = new kakao.maps.Map(container, options);
+    }, []);
+
+    return (
+        <React.Fragment>
+            <div id="map" style={ {width: '500px', height: '500px'} }>
+                Map Page
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Map;
