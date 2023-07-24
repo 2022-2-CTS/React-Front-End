6b19788aa65b9114ab5cce9985b1ebc2b7fb0666
leejyohan <86225337+leejyohan@users.noreply.github.com>
2023-07-25 00:21:02 +0900

    📝 Modify: ShareInfo 요소 추가

--- src/info/ShareInfo.js
+++ src/info/ShareInfo.js
@@ -3,7 +3,21 @@ import React from "react";
 const ShareInfo = () => {
   return (
     <div>
-        정보공유페이지
+      <div className="flex justify-center items-center text-xl">해운대구</div>
+      <div className="border-b-2 border-d9d9d9"></div>
+      <div>
+        <div>
+          <div className="inline-flex">행사이름</div>
+          <div className="inline-block">태그</div>
+          <div className="inline-block">작성일</div>
+        </div>
+        <div className="">
+          지도 이미지
+        </div>
+        <div>
+          글내용
+        </div>
+      </div>
     </div>
   );
 };
