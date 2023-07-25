47a7d5df09c9e789e36d37d180150335b21fbe03
이지호 <42643691+jiho9702@users.noreply.github.com>
2023-07-26 02:16:34 +0900

    ✨ Feat: 아이디 유효성 검사 + 비밀번호 일치 + 백엔드와 통신

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -6,11 +6,13 @@ const Login = () => {
     const [id, setId] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirm, setPasswordConfirm] = useState('')
+    const [idValidCheck, setIdValidCheck] = useState(false)
 
+    const passwordMatch = password == passwordConfirm
 
-    function complete(){
-        window.location.href="./signin"
-    }
+    const blankCheck = passwordConfirm == ''
+
+    const finish = idValidCheck == passwordMatch;
 
     function saveUserId(event){
         setId(event.target.value)
@@ -30,14 +32,32 @@ const Login = () => {
             sendValidId : id,
         }).then((req) => {
             console.log(req.data)
+            if (req.data == 'success'){
+                setIdValidCheck(true)
+                console.log("오!!")
+            }else{
+                setIdValidCheck(false)
+                console.log("오잉?")    
+            }
         }).catch((err) => {
             console.log(err)
         })
     }
 
-    const passwordMatch = password == passwordConfirm
-
-    const blankCheck = passwordConfirm == ''
+    function complete(){
+        if(passwordMatch){
+            console.log(id)
+            console.log(password)
+            axios.post("http://localhost:3004/api/index/signup",{
+                sendId : id,
+                sendPw : password,
+            }).then((req) => {
+                console.log(req.body)
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
 
     return (
         <div class="mx-6 my-24">
@@ -59,12 +79,18 @@ const Login = () => {
                     </button>
                 </div>
                 <div class="col-span-3">
-                    <p class="font-light text-sm font-bold text-green-600">사용할 수 있는 아이디 입니다.</p>
+                    {
+                        idValidCheck
+                        ?
+                        <p class="font-light text-sm font-bold text-green-600">사용할 수 있는 아이디 입니다.</p>
+                        :
+                        <p class="font-light text-sm font-bold text-rose-600">사용할 수 없는 아이디 입니다.</p>
+                    }
                 </div>
                 <div class="col-span-4 mt-4">
                     <label class="block">
                         <span class="block text-sm font-medium text-slate-700">비밀번호</span>
-                        <input type="text" name="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-lg sm:text-sm focus:ring-1" 
+                        <input type="password" name="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-lg sm:text-sm focus:ring-1" 
                             onChange={saveUserPw}
                         />
                     </label>
@@ -72,7 +98,7 @@ const Login = () => {
                 <div class="col-span-4">
                     <label class="block">
                         <span class="block text-sm font-medium text-slate-700">비밀번호 확인</span>
-                        <input type="text" name="password" class="mt-1 px-3 py-2 pr-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-lg sm:text-sm focus:ring-1" 
+                        <input type="password" name="password" class="mt-1 px-3 py-2 pr-1 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-lg sm:text-sm focus:ring-1" 
                             onChange={saveUserConfirmPw}
                         />
                     </label>
@@ -83,14 +109,23 @@ const Login = () => {
                     <div class="col-span-3" />
                     : 
                     <div class="col-span-3">
-                        {passwordMatch ? <p class="font-light text-sm font-bold text-rose-600">비밀번호가 일치합니다.</p> : <p class="font-light text-sm font-bold text-rose-600">비밀번호를 다시 확인해 주세요.</p>}
+                        {passwordMatch ? <p class="font-light text-sm font-bold text-green-600">비밀번호가 일치합니다.</p> : <p class="font-light text-sm font-bold text-rose-600">비밀번호를 다시 확인해 주세요.</p>}
                     </div>
                 }
                 <div class="col-span-4 mt-4">
-                    <button class="rounded-lg border-solid border-2 border-indigo-500 bg-indigo-500 w-full py-2"
+                    {
+                        finish
+                        ?
+                        <button class="rounded-lg border-solid border-2 border-indigo-500 bg-indigo-500 w-full py-2"
                             onClick={complete}>
-                        <p class="font-bold text-xl text-white">가입 완료</p>
-                    </button>
+                            <p class="font-bold text-xl text-white">가입 완료</p>
+                        </button>
+                        :
+                        <button class="rounded-lg border-solid border-2 border-indigo-200 bg-indigo-200 w-full py-2"
+                        >
+                            <p class="font-bold text-xl text-white">가입 실패</p>
+                        </button>
+                    }
                 </div>
             </div>
         </div>
