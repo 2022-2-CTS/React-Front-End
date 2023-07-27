3b6ca673b41b88a707e0d62c00a30f5d4d22c403
Miseon <soup7912@gmail.com>
2023-07-27 12:14:25 +0900

    ✅ Test: 임시 마커 클릭 시, 상세 페이지로 이동하는 테스트 코드 추가

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import Nav from "../component/BottomNav";
 
@@ -8,15 +9,48 @@ import Nav from "../component/BottomNav";
 const { kakao } = window; // window 내 kakao 객체를 빼와서 사용
 
 const Map = () => {
+    const navigate = useNavigate();
+
     useEffect(() => {
-        const container = document.getElementById("map");
-        const options = {
-            center: new kakao.maps.LatLng(35.14932, 129.11462, 16),
-            level: 6,
-        };
+        // kakao.js가 load됐을 때 메서드 호출
+        kakao.maps.load(() => {
+            const container = document.getElementById("map");
+            const options = {
+                center: new kakao.maps.LatLng(35.14932, 129.11462, 16),
+                level: 6,
+            };
 
 
-        const map = new kakao.maps.Map(container, options);
+            const map = new kakao.maps.Map(container, options);
+
+            // 주소-좌표 변환 객체 생성
+            var geocoder = new kakao.maps.services.Geocoder();
+
+            // 주소로 좌표 검색
+            geocoder.addressSearch('부산 해운대구 달맞이길65번길 154 지하2층 카린', function (result, status) {
+
+                // 검색 성공
+                if (status === kakao.maps.services.Status.OK) {
+
+                    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                    // 검색 결과 좌표를 이용해 마커 그리기
+                    var marker = new kakao.maps.Marker({
+                        map: map,
+                        position: coords
+                    });
+
+                    // 지도 중심을 마커로 이동
+                    map.setCenter(coords);
+
+                    // 마커 클릭이벤트 등록
+                    kakao.maps.event.addListener(marker, 'click', function () {
+                        navigate('/detail');
+                    });
+                }
+            });
+        })
+
     }, []);
 
     const categoryArray = ["뮤지컬", "연극", "공연·전시", "콘서트"]
