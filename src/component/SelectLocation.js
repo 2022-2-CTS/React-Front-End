58605e902bf67f212b6576f906a06c6621da310d
Miseon <soup7912@gmail.com>
2023-07-30 19:57:44 +0900

    💄 Style: 위치 선택 페이지 marker 적용

--- src/component/SelectLocation.js
+++ src/component/SelectLocation.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import LocationMarker from "../img/icon/location_select.svg";
+
 // 스크립트로 kakao map api를 심어서 가져오면, window 전역 객체에 들어가게 된다.
 // 함수형 컴포넌트에서는 바로 인식하지 못하므로, kakao 객체를 인지시키고자 상단에 선언해둔다.
 const { kakao } = window; // window 내 kakao 객체를 빼와서 사용
@@ -28,7 +30,11 @@ const SelectLocation = ({setTestResponse}) => {
 
             // 주소-좌표 변환 객체 생성
             const geocoder = new kakao.maps.services.Geocoder();
-            let marker = new kakao.maps.Marker();
+
+            // 마커 생성
+            const markerImage = new kakao.maps.MarkerImage(LocationMarker, new kakao.maps.Size(55, 55));
+            let marker = new kakao.maps.Marker({image: markerImage});
+            
             // 이전 마커 흔적 clear
             marker.setMap(null);
             
