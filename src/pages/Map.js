2e4e4146783d2c71d33b2bbefd2ae5b2af3b4ccc
Miseon <soup7912@gmail.com>
2023-08-03 17:38:04 +0900

    ✅ Test: 행사 데이터에 따라 지도에 마커 표시
    테스트 코드 작성함. 행사 목록 API 구현 완료 시 수정할 것.

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -9,12 +10,58 @@ import MarkerPlay from "../img/icon/marker_play_blue.svg";
 import MarkerExhibition from "../img/icon/marker_exhibition_green.svg";
 import MarkerConcert from "../img/icon/marker_concert_yellow.svg";
 
-const categoryArray = ["뮤지컬", "연극", "공연·전시", "콘서트"]
-const categoryColorArray = ["bg-red-500", "bg-blue-500", "bg-green-500", "bg-yellow-500"]
-
 // 스크립트로 kakao map api를 심어서 가져오면, window 전역 객체에 들어가게 된다.
 // 함수형 컴포넌트에서는 바로 인식하지 못하므로, kakao 객체를 인지시키고자 상단에 선언해둔다.
 const { kakao } = window; // window 내 kakao 객체를 빼와서 사용
+
+async function getEventInfoRequest(url, setter) {
+    await axios.get(url)
+    .then((response) => {
+        setter(...response);
+    });
+}
+
+async function getEventInfo(musicalsSetter, playsSetter, exhibitionsSetter, concertsSetter) {
+    await getEventInfoRequest('http://localhost:3004/api/event/musicals', musicalsSetter)
+    await getEventInfoRequest('http://localhost:3004/api/event/plays', playsSetter)
+    await getEventInfoRequest('http://localhost:3004/api/event/exhibitions', exhibitionsSetter)
+    await getEventInfoRequest('http://localhost:3004/api/event/concerts', concertsSetter)
+}
+
+function test(navigate, map, geocoder, data, markerImage) {
+    // for(let i = 0; i < data.length; i++) {
+        geocoder.addressSearch(data.location, function (result, status) {
+            if (status === kakao.maps.services.Status.OK) {
+                let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                // 검색 결과 좌표를 이용해 마커 그리기
+                let marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords,
+                    image: new kakao.maps.MarkerImage(markerImage, new kakao.maps.Size(55, 55))
+                });
+
+                // 지도 중심을 마커로 이동
+                map.setCenter(coords);
+
+                // 마커 클릭이벤트 등록
+                kakao.maps.event.addListener(marker, 'click', function () {
+                    // 클릭한 마커가 갖고 있는 행사 정보를 함께 보내는 라우팅
+                    navigate('/detail', {
+                        state: {
+                            title: data.title,
+                            location: data.location,
+                            duration: data.startDate + ' ~ ' + data.endDate,
+                            time: data.time,
+                            price: data.price,
+                            src: data.imgSrc
+                        }
+                    });
+                });
+            }
+        });
+    // }
+}
 
 const Map = () => {
     const navigate = useNavigate();
@@ -27,7 +74,23 @@ const Map = () => {
     */
     let [selectedCategoryIndex, setselectedCategoryIndex] = useState(0);
 
+    // 행사 목록 불러오기
+    let [musicalsData, setMusicalsData] = useState(null);
+    let [playsData, setPlaysData] = useState(null);
+    let [exhibitionsData, setExhibitionsData] = useState(null);
+    let [concertsData, setConcertsData] = useState(null);
+
+    const categoryArray = ["뮤지컬", "연극", "공연·전시", "콘서트"];
+    const categoryColorArray = ["bg-red-500", "bg-blue-500", "bg-green-500", "bg-yellow-500"];
+
     useEffect(() => {
+        // getEventInfo(
+        //     setMusicalsData,
+        //     setPlaysData,
+        //     setExhibitionsData,
+        //     setConcertsData
+        // );
+
         // kakao.js가 load됐을 때 메서드 호출
         kakao.maps.load(() => {
             const container = document.getElementById("map");
@@ -36,46 +99,52 @@ const Map = () => {
                 level: 6,
             };
 
-
             const map = new kakao.maps.Map(container, options);
 
             // 주소-좌표 변환 객체 생성
-            var geocoder = new kakao.maps.services.Geocoder();
+            const geocoder = new kakao.maps.services.Geocoder();
+
+            var testData = {
+                title: "METAPHORIC SCENERY : 은유적 풍경",
+                location: "부산 해운대구 달맞이길65번길 154 지하2층 카린갤러리",
+                startDate: "2023.07.07",
+                endDate: "2023.08.27",
+                time: "10:00 - 18:00 (월요일 휴무)",
+                price: "무료"
+            }
+
+            test(navigate, map, geocoder, testData, MarkerMusical);
 
             // 주소로 좌표 검색
-            geocoder.addressSearch('부산 해운대구 달맞이길65번길 154 지하2층 카린', function (result, status) {
+            // geocoder.addressSearch('부산 해운대구 달맞이길65번길 154 지하2층 카린', function (result, status) {
 
-                // 검색 성공
-                if (status === kakao.maps.services.Status.OK) {
+            //     // 검색 성공
+            //     if (status === kakao.maps.services.Status.OK) {
 
-                    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            //         var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-                    // 검색 결과 좌표를 이용해 마커 그리기
-                    var marker = new kakao.maps.Marker({
-                        map: map,
-                        position: coords,
-                        image: new kakao.maps.MarkerImage(MarkerMusical, new kakao.maps.Size(55, 55))
-                    });
+            //         // 검색 결과 좌표를 이용해 마커 그리기
+            //         var marker = new kakao.maps.Marker({
+            //             map: map,
+            //             position: coords,
+            //             image: new kakao.maps.MarkerImage(MarkerMusical, new kakao.maps.Size(55, 55))
+            //         });
 
-                    // 지도 중심을 마커로 이동
-                    map.setCenter(coords);
+            //         // 지도 중심을 마커로 이동
+            //         map.setCenter(coords);
 
-                    // 마커 클릭이벤트 등록
-                    kakao.maps.event.addListener(marker, 'click', function () {
-                        // 클릭한 마커가 갖고 있는 행사 정보를 함께 보내는 라우팅
-                        navigate('/detail', {
-                            state: {
-                                title: "METAPHORIC SCENERY : 은유적 풍경",
-                                location: "카린갤러리",
-                                duration: "2023.07.07 ~ 2023.08.27",
-                                time: "10:00 - 18:00 (월요일 휴무)",
-                                price: "무료"
-                            }
-                        });
-                    });
-                }
-            });
-        })
+            //         // 마커 클릭이벤트 등록
+            //         kakao.maps.event.addListener(marker, 'click', function () {
+            //             // 클릭한 마커가 갖고 있는 행사 정보를 함께 보내는 라우팅
+            //             navigate('/detail', {
+            //                 state: {
+
+            //                 }
+            //             });
+            //         });
+            //     }
+            // });
+        });
 
     }, []);
 
