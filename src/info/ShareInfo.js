9e10e36d47d82e89d04311037f72a4d3b1c3ba0b
Miseon <soup7912@gmail.com>
2023-08-08 17:19:09 +0900

    🐛 Bug: DOM 객체가 생기기 전에 map을 생성하여 오류가 발생하는 상황
    게시판 페이지로 이동할 때 버그가 발생함. 새로고침 시 정상 작동함.

--- src/info/ShareInfo.js
+++ src/info/ShareInfo.js
@@ -17,15 +17,15 @@ function write() {
   - postLocation : 해당 post의 주소 문자열 (DB에서 추출)
 */
 const PostMap = (postId, postLocation) => {
-  kakao.maps.load(() => {
-    let mapContainer = document.getElementById(postId),
+  kakao.maps.load(async () => {
+    const mapContainer = await document.getElementById(postId),
       mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 3
       };
 
-    let map = new kakao.maps.Map(mapContainer, mapOption);
-    let geocoder = new kakao.maps.services.Geocoder();
+    const map = await new kakao.maps.Map(mapContainer, mapOption);
+    const geocoder = new kakao.maps.services.Geocoder();
 
     geocoder.addressSearch(postLocation, function (result, status) {
       if (status === kakao.maps.services.Status.OK) {
@@ -60,20 +60,21 @@ let post = [1, 2, 3, 4, 5]
 
 const ShareInfo = () => {
   return (
-    <div>
+    <React.Fragment>
       <div className="flex justify-center items-center text-xl">해운대구</div>
       <div className="border-b-2 border-d9d9d9"></div>
 
       {
         post.map((index) => {
           return (
-            <div>
+            <div key={index}>
               <div>
                 <div className="inline-flex">행사이름</div>
                 <div className="inline-block">태그</div>
                 <div className="inline-block">작성일</div>
               </div>
-              <div className="flex justify-center items-center w-3/4 h-48 bg-indigo-200 place-center">
+              <div
+              className="flex justify-center items-center w-3/4 h-48 bg-indigo-200 place-center">
                   {/* TEST : postId 작성하고, DB에서 해당 글의 postLocation 입력 바람 */}
                   { PostMap("test", '부산 해운대구 달맞이길65번길 154 지하2층 카린') }
               </div>
@@ -86,7 +87,7 @@ const ShareInfo = () => {
         })
       }
       <Write className="fixed bottom-0 right-0" onClick={write} ></Write>
-    </div>
+    </React.Fragment>
   );
 };
 
