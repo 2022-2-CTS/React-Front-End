51b22ced96a5e35cbced1e257fd0c0c93a682e06
Miseon <soup7912@gmail.com>
2023-08-13 16:03:12 +0900

    💄 Style: 상세페이지, Map 요소에 fade 애니메이션 적용

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -242,7 +242,7 @@ const Map = () => {
             </div>
 
             {/* map */}
-            <div id="map" className="w-screen h-screen"></div>
+            <div id="map" className="animated-fade w-screen h-screen"></div>
 
             {/* bottom nav bar */}
             <Nav />
